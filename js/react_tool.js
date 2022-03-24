1fde78fe0935235d903ca7b148b281f3ae159abc
a19anjst <62884743+a19anjst@users.noreply.github.com>
2022-03-24 02:57:08 +0100

    tool layout started

--- js/react_tool.js
+++ js/react_tool.js
@@ -29,7 +29,10 @@ class ReactApp extends React.Component {
   return (
     <main>
        <Modal show={this.state.show} handleClose={this.hideModal}>
-         <p>Modal</p>
+         <p id="reacttool_title">Textstorlek: </p>
+         <input type="radio" value="16px" name="fontsize" /> 16px
+         <input type="radio" value="18px" name="fontsize" /> 18px
+         <input type="radio" value="20px" name="fontsize" /> 20px
        </Modal>
        <button id="buttonOpen" onClick={this.handleClick}>
         {this.state.show ? 'Textverktyg' : 'Textverktyg'}
