bbadada97855f08b97926df2541fb7bf0d478c80
RindiRio95 <a19anjst@student.his.se>
2022-04-17 14:09:42 +0200

    Create dbtext.php

---
+++ dbtext.php
@@ -0,0 +1,13 @@
+<?php
+include_once ('db_connection.php');
+
+$sql = 'SELECT* FROM texttable order by RAND() LIMIT 1';
+
+$result = mysqli_query($conn,$sql);
+$json_array= array();
+while($row = mysqli_fetch_assoc($result)){
+  $json_array[]=$row;
+}
+
+echo json_encode($json_array);
+?>
