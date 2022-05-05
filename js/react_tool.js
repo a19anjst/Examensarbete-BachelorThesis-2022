b82ddb9800753801202c3528ef67faa2dd3dd5c9
a19anjst <62884743+a19anjst@users.noreply.github.com>
2022-05-05 17:45:19 +0200

    removed php/mysql
    Now it is pure HTML text instead so to not disturb test results

--- js/react_tool.js
+++ js/react_tool.js
@@ -146,7 +146,16 @@ class ReactApp extends React.Component {
       <div className="textblock" id="textblock1">
           <div className="content_second" style={getcolor}>
             <div className="content_dir1" style={getfontsize}>
-            <Data/>
+            <h1>Lorem Ipsum</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis sapien eget purus sollicitudin blandit. Nunc ut orci neque. Suspendisse sit amet ante quis ex faucibus dictum. Proin odio magna, suscipit sed sollicitudin ut, suscipit ac velit. Phasellus purus nulla, faucibus a volutpat quis, interdum ut orci. Nulla facilisi. Etiam consectetur sed nisl et mattis. Pellentesque nec libero nisl. Nulla rutrum sem sed elit congue rhoncus. In consectetur ipsum id lectus commodo tempus. Cras elementum quam vitae lacinia porttitor. Cras porttitor dolor eget justo dapibus finibus.</p>
+
+            <p>Maecenas arcu nulla, mollis in suscipit in, consequat vitae turpis. Ut mollis, velit id vulputate gravida, orci tortor malesuada ex, quis mattis tortor nulla a est. Fusce mi sem, tincidunt consectetur malesuada auctor, luctus nec mauris. Maecenas nec cursus eros, in viverra urna. Integer semper, nibh eu ultricies facilisis, lectus ante pretium felis, nec dictum nisi orci egestas turpis. Etiam ut augue ut sapien ultricies feugiat sed vitae velit. Nullam et tincidunt magna, vitae varius ex. Sed eros metus, porta vel viverra eget, cursus eu nulla. Etiam volutpat turpis in augue dictum, at commodo eros gravida. Pellentesque ut urna justo.</p>
+
+            <p>Suspendisse pretium euismod scelerisque. Mauris arcu ipsum, dictum ac lacus quis, egestas ornare ipsum. Nullam finibus nec lorem et finibus. Integer rutrum dapibus lacus et ultrices. Nullam rhoncus at est eu accumsan. Nunc ut mauris ullamcorper, tincidunt libero quis, ornare risus. Sed posuere tempus nunc, at consequat urna cursus eget. Proin id ipsum diam. Nullam tincidunt blandit laoreet. Nullam accumsan ex rutrum, molestie nisl sit amet, sollicitudin elit. Nulla nibh dolor, vehicula id quam ut, sollicitudin auctor nulla. Integer pretium ut orci vestibulum luctus. Donec aliquet justo justo, sit amet mollis urna ultricies eu. Suspendisse a tellus porta, pellentesque purus id, mollis nibh. Proin imperdiet eleifend sem.</p>
+
+            <p>Quisque tristique nisl a lacus feugiat lacinia. Vestibulum posuere fringilla tellus non vehicula. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque suscipit tincidunt tempus. Duis suscipit est mi, eget tincidunt turpis cursus ac. Cras a varius lacus. Donec dapibus diam nisi. Sed iaculis auctor quam ut tempor. Pellentesque varius pharetra augue quis bibendum. Sed lobortis nisi eget nulla efficitur lobortis.</p>
+
+            <p>Donec quis enim facilisis, rutrum velit vitae, congue eros. Suspendisse vel mauris commodo, interdum nibh sed, ultricies nibh. Praesent fermentum vitae nibh non ultricies. Praesent gravida venenatis tristique. Duis eget porta ante. Integer blandit, neque in luctus commodo, sapien nisi volutpat turpis, at euismod sapien turpis a est. Morbi non luctus quam. Donec pharetra, metus sit amet ultricies maximus, magna odio rhoncus mi, sed efficitur risus purus in orci. Phasellus ac urna libero. Sed aliquam blandit tortor. Duis odio nisl, vulputate vitae aliquet quis, semper non erat. Integer sed magna turpis.</p>
             </div>
           </div>
 
@@ -166,33 +175,3 @@ const Modal = ({ handleClose, show, children }) => {
     </div>
   );
 };
-
-const Data = () =>{
-  const [data, setItem] = React.useState([]);
-
-
-  React.useEffect(() => {
-    fetch("../Examensarbete-BachelorThesis-2022/dbtext.php")
-      .then(res => res.json())
-      .then(
-        (result)=>{
-        setItem(result);
-        //console.log(date)
-        }
-      )
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-  return(
-    <div>
-      {data.map((item) => (
-
-        <div key={item.ID}><UnsafeComponent html={item.Text}/></div>
-      ))}
-    </div>
-  )
-}
-function UnsafeComponent({ html }) {
-  return <div dangerouslySetInnerHTML={{ __html: html }} />;
-}
